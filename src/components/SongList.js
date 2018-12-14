5d7370102545725d3f527c793d06fa98cf21cd89
Alejandro <alejandrochang10@gmail.com>
2018-12-14 14:21:52 -0800

    song detail component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList = () => {
-    return this.props.songs.map((song) => {
+    return this.props.songs.map(song => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
       );
     });
-  }
+  };
   render() {
-    return <div>{this.renderList()}</div>
+    return (
+      <div className="ui divided list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {songs: state.songs };
-}
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
 
 // const mapDispatchToProps = (props) => {
-
 // }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong: selectSong }
+)(SongList);

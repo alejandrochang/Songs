f29cec8a66d17c2c61fc0e442e6d2f30ee67feba
Alejandro <alejandrochang10@gmail.com>
2018-12-14 13:13:36 -0800

    adding the provider to our application

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
   return [
-    {title: 'No Scrubs', duratin: '4:05'},
-    {title: 'Macarena', duratin: '2:30'},
-    {title: 'All-Star', duratin: '3:15'},
-    {title: 'I want it that way', duratin: '1:45'}
+    {title: 'No Scrubs', duration: '4:05'},
+    {title: 'Macarena', duration: '2:30'},
+    {title: 'All-Star', duration: '3:15'},
+    {title: 'I want it that way', duration: '1:45'}
   ];
 };
 
